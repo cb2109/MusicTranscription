396333e31ba8c556c4f0d7ad88061b8fa40f3366
Chris Bates <blokeyking@gmail.com>
2014-03-09 19:47:29 +0000

    Begun the Multirate filter bank

---
+++ src/user/cb2109/musictranscription/processing/f0analysis/filterbanks/Decimator.java
@@ -0,0 +1,11 @@
+package user.cb2109.musictranscription.processing.f0analysis.filterbanks;
+
+/**
+ * Author: Christopher Bates
+ * Date: 09/03/14
+ */
+class Decimator {
+
+
+
+}
---
+++ src/user/cb2109/musictranscription/processing/f0analysis/filterbanks/MultiRateFilterBank.java
@@ -0,0 +1,20 @@
+package user.cb2109.musictranscription.processing.f0analysis.filterbanks;
+
+import java.util.List;
+
+/**
+ * Author: Christopher Bates
+ * Date: 09/03/14
+ */
+public class MultiRateFilterBank {
+
+    List<Decimator> decimators;
+
+    public MultiRateFilterBank() {
+
+    }
+
+
+
+
+}
